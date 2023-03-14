d3bb80977219b443ec2b4505026b535ac1f64b87
kingof7 <vlfl0@naver.com>
2023-03-14 23:11:09 +0900

    spring boot + jpa + mysql server initialized

---
+++ src/main/java/com/todo/task/TaskItem.java
@@ -0,0 +1,53 @@
+package com.todo.task;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name="task_item")
+public class TaskItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @NotNull(message = "title is required")
+    private String title;
+    private boolean done;
+
+    public TaskItem() {
+    }
+
+    public TaskItem(String title) {
+        this.title = title;
+        this.done = false;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
+}
